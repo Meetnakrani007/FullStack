013c266f23235e49a83a8ef3c30b83b26a89c0e6
Meet Nakrani <nakranimeet2005@gmail.com>
2024-10-29 15:58:54 +0530

    part-11

--- part-11/app.js
+++ part-11/app.js
@@ -14,3 +14,29 @@ function three()
 }
 
 three();
+// call back hell
+
+h1 = document.querySelector("h1");
+function changeColor(color,delay,nextColorChange)
+{
+    setTimeout(()=>
+    {
+        h1.style.color= color ;
+        nextColorChange();
+    },delay);
+   
+}
+changeColor ("red", 1000,()=>
+{
+    changeColor ("orange", 1000,()=>
+    {
+        changeColor ("green", 1000,()=>
+        {
+            changeColor ("yellow", 1000,()=>
+            {
+                changeColor ("blue", 1000,)
+            });
+        })
+    });
+});
+
