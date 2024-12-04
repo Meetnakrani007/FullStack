0a6aedc357c9f1efb1d02c8aeebf430c8e620c93
Meet Nakrani <nakranimeet2005@gmail.com>
2024-12-04 15:09:09 +0530

    backend

---
+++ Backend/part-1/math.js
@@ -0,0 +1,13 @@
+let sum = (a,b) => a+b;
+let mul = (a,b) => a*b;
+let div = (a,b) => a/b;
+let sub = (a,b) => a-b;
+
+let obj = {
+    sum: sum,
+    mul: mul,
+    div: div,
+    sub: sub
+
+}
+module.exports = obj;
