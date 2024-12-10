5741f2584911f47383d269d013e60f44df6e64a5
Meet Nakrani <nakranimeet2005@gmail.com>
2024-12-10 13:47:46 +0530

    SQL

--- Backend/SQLCLASS/index.js
+++ Backend/SQLCLASS/index.js
@@ -1,6 +1,20 @@
 const { faker } = require('@faker-js/faker');
 
+const methodOverride = require("method-override");
+const path = require("path");
+
+
 const mysql = require ('mysql2');
+
+const express = require("express");
+const app = express();
+
+
+app.use(express.json());
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views")); 
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({extended :true}));
 
 const connection = mysql.createConnection({
 host: 'localhost',
@@ -9,9 +23,78 @@ database: 'delta_app',
 password: 'Dikime@123'
 });
 
-let q = "INSERT INTO user (id, username, email, password) VALUES ?";
 
-let data=[];
+app.listen("8080",(req,res)=>
+{
+    console.log("listning port on 8080");
+});
+
+//HOME ROUTE
+app.get("/",(req,res)=>
+{
+let q="SELECT count(*) FROM user";
+try
+{
+    connection.query(q,(err,result)=>
+{
+    let count = result[0]["count(*)"];
+    if(err) throw err
+    res.render("home.ejs",{ count });
+    
+
+});
+}
+catch(err)
+{
+    console.log(err);
+    res.send("some error in data base");
+}
+});
+
+
+//SHOW ROUTE
+app.get("/user",(req,res)=>
+{
+let q=`SELECT * FROM user`;
+try
+{
+    connection.query(q,(err,users)=>
+{
+    if(err) throw err
+    res.render("show.ejs",{ users });
+});
+}
+catch(err)
+{
+    console.log(err);
+    res.send("some error in data base");
+}
+});
+
+//EDIT ROUTE 
+app.get("/user/:id/edit",(req,res)=>
+{
+let { id } = req.params;
+let q=`SELECT * FROM user WHERE id = '${id}'`;
+try
+{
+    connection.query(q,(err,result)=>
+{
+    if(err) throw err
+   
+    let user = result[0];
+    res.render("edit.ejs",{ user });
+    
+});
+}
+catch(err)
+{
+    console.log(err);
+    res.send("some error in data base");
+}
+});
+
+
 
 let getRandomUser = ()=> {
     return [
@@ -21,22 +104,116 @@ let getRandomUser = ()=> {
       faker.internet.password(),
     ];
   };
-  
 
-for(let i=0;i<100;i++)
+//PATCH ROUTE 
+
+app.patch("/user/:id",(req,res)=>
 {
-    data.push(getRandomUser());
-}
-try
+    let { id } = req.params;
+    let{password : formPass , username : updateUser} = req.body;
+    let q =`SELECT * FROM user WHERE id = '${id}'`;
+    try
 {
-    connection.query(q,[data],(err,res)=>
+    connection.query(q,(err,result)=>
 {
     if(err) throw err
-    console.log(res);
+    let user = result[0];
+   
+    if(formPass != user.password)
+    {
+        res.send("Password is wrong");
+    }
+    else
+    {
+        let q2 =`UPDATE user SET username = '${updateUser}' WHERE id = '${id}'`; 
+        connection.query(q2,(err,result)=>
+    {
+        if(err) throw err
+        res.redirect("/user");
+    });
+    }
+});
+}catch(err)
+{
+    console.log(err);
+    res.send("some error in data base");
+}
+});
+
+//DELETE ROUTE 
+app.get("/user/:id/delete",(req,res)=>
+{
+    let { id } = req.params;
+    let q=`SELECT * FROM user WHERE id = '${id}'`;
+try
+{
+    connection.query(q,(err,result)=>
+{
+    if(err) throw err
+   
+    let user = result[0];
+    res.render("delete.ejs",{ user });
+    
 });
 }
 catch(err)
 {
     console.log(err);
-}
-connection.end();
+    res.send("some error in data base");
+}  
+});
+
+
+
+
+app.delete("/user/:id",(req,res)=>
+{
+    let { id } = req.params;
+    let{password : formPass} = req.body;
+    let q =`SELECT * FROM user WHERE id = '${id}'`;
+    connection.query(q,(err,result)=>{
+        if(err) throw err
+        try
+        {
+            let user = result[0];
+            if(formPass != user.password)
+            {
+                res.send("Your Password is Wrong");        
+            }
+            else
+            {
+                let q2 = `DELETE FROM user WHERE id = '${id}'`;
+                connection.query(q2,(err,result)=>{
+                    
+                    if(err) throw err
+                    res.redirect("/user");
+                
+                });
+                
+            }
+        }
+        catch(err){
+            console.log(err);
+            res.send("some error in data base");
+        }
+    
+
+    });
+    
+       
+});
+//ADD ROUTE
+app.get("/user/add",(req,res)=>
+{
+    res.render("add.ejs");
+});
+app.post("/user/add",(req,res)=>
+{
+    let {id: formId , username: formUser , email: formEmail , password: formPass } = req.body;
+    let q = "INSERT INTO user (id ,username ,email ,password) VALUES (? , ? , ? , ?)";
+    let arr =[formId,formUser,formEmail,formPass];
+    connection.query(q,arr,(err,result)=>
+    {
+      res.redirect("/user");
+    });
+});
