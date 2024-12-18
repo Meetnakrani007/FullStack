be886a70ef526630ffc770f87dc9f56cbc4bb227
Meet Nakrani <nakranimeet2005@gmail.com>
2024-12-18 20:41:04 +0530

    Error-Middleware

--- Backend/Middleware/index.js
+++ Backend/Middleware/index.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const app =  express();
+const ExpressError = require("./ExpressError");
 
-app.use((req, res , next)=>{
-    
-    req.time = new Date(Date.now()).toString();
-
-    console.log(req.path , req.method , req.hostname,req.time);
+const checkToken = ("/api",(req,res,next)=>
+{
+    let {token} = req.query;
+    if(token === "giveaccess")
+    {
     next();
+    }
+    throw new ExpressError(401,"ACCESS DENIED");
+})
+app.get("/api",checkToken,(req,res)=>
+{
+    res.send("data");
 });
 app.get("/",(req,res)=>
 {
@@ -15,7 +22,14 @@ app.get("/",(req,res)=>
 app.get("/random",(req,res)=>
 {
     res.send("I am random path ");
+});
+app.get("/admin",(req,res)=>{
+    throw new ExpressError(403,"Access to admin is ForBidden");
+});
+app.use((err,req,res,next)=>{
+    let{status = 401,message = "Some Error"} = err;
+    res.status(status).send(message);
 })
 app.listen(8080,()=>{
     console.log("listening on 8080");
-})
+});
