0dbd6770068015bfeaf2e9178486126659330e9b
Meet Nakrani <nakranimeet2005@gmail.com>
2025-01-17 10:58:16 +0530

    Major-Project

--- Major Project /Cookie/server.js
+++ Major Project /Cookie/server.js
@@ -2,31 +2,46 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const cookieParser  = require("cookie-parser");
+const session = require("express-session");
 
+app.use(session({secret : "mysupersecretstring", resave: false , saveUninitialized : true}));
 
-app.use(cookieParser("secretcode"));
-
-app.get("/getcookies",(req,res)=>{
-res.cookie("name","Meet");
-res.cookie("age","19");
-res.send("sent you some cookie");
-});
-
-app.get("/getsignedcookies",(req,res)=>{
-    res.cookie("name","Meet",{signed:true});
-    res.send("success");
-    });
-
-app.get("/verify",(req,res)=>{
-    console.log(req.signedCookies);
-    res.send("verified");
+app.get("/reqcount",(req,res)=>{
+    if(req.session.count)
+    {
+        req.session.count++;
+    }else{
+        req.session.count = 1;
+    }
+   
+    res.send(`Req count is ${req.session.count}`);
 });
 
 
-app.get("/",(req,res)=>{
-    console.dir(req.cookies);
-    res.send("Hi i am root");
-});
+
+
+
+// app.get("/getcookies",(req,res)=>{
+// res.cookie("name","Meet");
+// res.cookie("age","19");
+// res.send("sent you some cookie");
+// });
+
+// app.get("/getsignedcookies",(req,res)=>{
+//     res.cookie("name","Meet",{signed:true});
+//     res.send("success");
+//     });
+
+// app.get("/verify",(req,res)=>{
+//     console.log(req.signedCookies);
+//     res.send("verified");
+// });
+
+
+// app.get("/",(req,res)=>{
+//     console.dir(req.cookies);
+//     res.send("Hi i am root");
+// });
 app.listen(3000,()=>{
 
     console.log("app listning on 3000 ");
