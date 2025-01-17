b33825d2e036acc4f352ff85cc18166932be8ad9
Meet Nakrani <nakranimeet2005@gmail.com>
2025-01-17 11:54:42 +0530

    Major-project

--- Major Project /Cookie/server.js
+++ Major Project /Cookie/server.js
@@ -3,22 +3,50 @@ const app = express();
 const path = require("path");
 const cookieParser  = require("cookie-parser");
 const session = require("express-session");
+const flash = require("connect-flash");
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
 
 app.use(session({secret : "mysupersecretstring", resave: false , saveUninitialized : true}));
+app.use(flash());
 
-app.get("/reqcount",(req,res)=>{
-    if(req.session.count)
+app.get("/ragister",(req,res)=>
+{ 
+    let {name = "anonymus"} = req.query
+    req.session.name = name;
+    if(name=="anonymus")
     {
-        req.session.count++;
-    }else{
-        req.session.count = 1;
-    }
-   
-    res.send(`Req count is ${req.session.count}`);
+        req.flash("success","Some error occured");
+    }else
+   { req.flash("error","New user ragister successfully!");}
+    res.redirect("/hello");
+
 });
 
 
+app.get("/hello",(req,res)=>{
+    res.locals.successMsg = req.flash("success");
+    res.locals.errorMsg = req.flash("error");
 
+    res.render("page.ejs",{ name: req.session.name });
+})
+
+
+
+
+
+
+
+// app.get("/reqcount",(req,res)=>{
+//     if(req.session.count)
+//     {
+//         req.session.count++;
+//     }else{
+//         req.session.count = 1;
+//     }
+   
+//     res.send(`Req count is ${req.session.count}`);
+// });
 
 
 // app.get("/getcookies",(req,res)=>{
