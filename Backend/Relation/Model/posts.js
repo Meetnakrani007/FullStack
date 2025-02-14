3a893ab8b97cc087477611c02d74e347b0112632
Meet Nakrani <nakranimeet2005@gmail.com>
2025-02-14 08:11:22 +0530

    Delete

--- Backend/Relation/Model/posts.js
+++ Backend/Relation/Model/posts.js
@@ -1,40 +1,41 @@
 const mongoose = require("mongoose");
-const {Schema} = mongoose;
-main().then(()=>{
+const { Schema } = mongoose;
+main()
+  .then(() => {
     console.log("Mongoose is working");
-}).catch(err => console.log(err));
+  })
+  .catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/relationDemo');
+  await mongoose.connect("mongodb://127.0.0.1:27017/relationDemo");
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
 const userSchema = new Schema({
-    username : String,
-    email : String,
+  username: String,
+  email: String,
 });
 const postSchema = new Schema({
-    content: String,
-    likes: Number,
-    user:
-    {
-        type: Schema.Types.ObjectId,
-        ref : "User",
-    }
+  content: String,
+  likes: Number,
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
 });
-const User = mongoose.model("User",userSchema);
-const Post = mongoose.model("Post",postSchema);
+const User = mongoose.model("User", userSchema);
+const Post = mongoose.model("Post", postSchema);
 
 // const addUser = async()=>{
 //     let user1 = await User.findOne({username: "Meet"});
 //     let post2 = new Post({
 //         content:" Ba bye :)",
 //         likes: 666,
-        
+
 //     });
 //     post2.user = user1;
-    
+
 //     await user1.save();
 //     await post2.save();
 // }
