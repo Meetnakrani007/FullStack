45feaab0fc490d9bfa743f989f17311047416c05
Meet Nakrani <nakranimeet2005@gmail.com>
2025-03-02 11:12:47 +0530

    react

---
+++ React/basic-react-app/src/ProductGroup.jsx
@@ -0,0 +1,12 @@
+import Product from "./Product";
+
+function ProductGroup() {
+  return (
+    <>
+      <Product />
+      <Product />
+      <Product />
+    </>
+  );
+}
+export default ProductGroup;
---
+++ React/basic-react-app/src/Title.jsx
@@ -0,0 +1,4 @@
+function Title() {
+  return <h1>I m the title</h1>;
+}
+export default Title;
